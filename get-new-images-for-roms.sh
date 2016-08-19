3c642a881d688054c53f32dc3bb87c848d4c5164
Adrian Moisey <adrian@changeover.za.net>
2016-08-19 20:09:24 +0200

    Add -use_nointro_name for better naming

--- get-new-images-for-roms.sh
+++ get-new-images-for-roms.sh
@@ -1,3 +1,3 @@
 for system in megadrive  n64  nes  psx  snes; do
-    ~/gocode/bin/scraper -workers=4 -thumb_only=true -image_dir files/pi/.emulationstation/downloaded_images/$system/ -image_path=~/.emulationstation/downloaded_images/$system/ -rom_dir=files/pi/RetroPie/roms/$system/ -output_file=files/pi/.emulationstation/gamelists/$system/gamelist.xml
+    ~/gocode/bin/scraper -use_nointro_name=false -workers=4 -thumb_only=true -image_dir files/pi/.emulationstation/downloaded_images/$system/ -image_path=~/.emulationstation/downloaded_images/$system/ -rom_dir=files/pi/RetroPie/roms/$system/ -output_file=files/pi/.emulationstation/gamelists/$system/gamelist.xml
 done
